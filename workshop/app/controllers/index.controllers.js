d87c415e6c96f91a07f7f56616e0011821a612c9
tanakorno <tanakorn_o@cmu.ac.th>
2016-05-27 06:30:20 +0700

    add login

--- workshop/app/controllers/index.controllers.js
+++ workshop/app/controllers/index.controllers.js
@@ -3,9 +3,17 @@ const dbconfig = require('../../config/admin');
 var db = pgp(dbconfig);
 
 exports.main = function(req, res){
-    res.render('index', {});
+    res.render('index');
 }
 
 exports.shop = function(req, res){
-    res.render('shop', {});
+    res.render('shop');
+}
+
+exports.login = function(req, res){
+    res.render('login');
+}
+
+exports.single = function(req, res){
+    res.render('single');
 }
--- workshop/app/routes.js
+++ workshop/app/routes.js
@@ -1,7 +1,16 @@
 var indexCtrl = require('./controllers/index.controllers');
+var registerCtrl = require('./controllers/register.controllers');
 
 module.exports = function(app){
-        app.get('/main', indexCtrl.main);
+        app.get('/', indexCtrl.main);
         app.get('/shop', indexCtrl.shop);
+        app.get('/login', indexCtrl.login);
+        app.get('/single', indexCtrl.single);
+        app.get('/register', registerCtrl.register);
+        
+        app.post('/create', registerCtrl.createUser);
+        app.post('/checkin', registerCtrl.checkLogin);
 }
 
+// input#username.validate(type='text', name='username', placeholder = 'username')
+// input#password.validate(type='password', name='password', placeholder ='password')
