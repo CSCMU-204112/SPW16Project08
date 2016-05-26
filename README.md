49a3d734b5ed70e384f4009482088bd8bd15fc34
tanakorno <tanakorn_o@cmu.ac.th>
2016-05-26 17:41:40 +0700

    update

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # SPW16Project08
-
-npm install express --save
-npm install morgan --save
-npm install body-parser --save
--- workshop/app/routes.js
+++ workshop/app/routes.js
@@ -1,5 +1,11 @@
+var memberCtrl = require('./controllers/member.controllers');
+
 module.exports = function(app){
     app.get('/', function(req, res){
         res.send('Working');
     });
+
+    app.route('/member')
+    .get(memberCtrl.getAll)
+    .post(memberCtrl.insert);
 }
