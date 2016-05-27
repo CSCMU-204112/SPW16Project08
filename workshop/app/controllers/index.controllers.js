09432180cd47b177e8d4487391437725adc19d0b
phichanya-boonmee <phichanya_boonmee@cmu.ac.th>
2016-05-27 12:52:21 +0700

    Last Update

--- workshop/app/controllers/index.controllers.js
+++ workshop/app/controllers/index.controllers.js
@@ -17,3 +17,48 @@ exports.login = function(req, res){
 exports.single = function(req, res){
     res.render('single');
 }
+
+exports.single2 = function(req, res){
+    res.render('single2');
+}
+
+exports.single3 = function(req, res){
+    res.render('single3');
+}
+
+exports.single4 = function(req, res){
+    res.render('single4');
+}
+
+exports.single5 = function(req, res){
+    res.render('single5');
+}
+
+exports.single6 = function(req, res){
+    res.render('single6');
+}
+exports.single7 = function(req, res){
+    res.render('single7');
+}
+
+exports.single8 = function(req, res){
+    res.render('single8');
+}
+
+exports.single9 = function(req, res){
+    res.render('single9');
+}
+
+
+exports.single10 = function(req, res){
+    res.render('single10');
+
+}
+
+exports.single11 = function(req, res){
+    res.render('single11');
+}
+
+exports.single12 = function(req, res){
+    res.render('single12');
+}
--- workshop/app/routes.js
+++ workshop/app/routes.js
@@ -6,6 +6,17 @@ module.exports = function(app){
         app.get('/shop', indexCtrl.shop);
         app.get('/login', indexCtrl.login);
         app.get('/single', indexCtrl.single);
+        app.get('/single2', indexCtrl.single2);
+        app.get('/single3', indexCtrl.single3);
+        app.get('/single4', indexCtrl.single4);
+        app.get('/single5', indexCtrl.single5);
+        app.get('/single6', indexCtrl.single6);
+        app.get('/single7', indexCtrl.single7);
+        app.get('/single8', indexCtrl.single8);
+        app.get('/single9', indexCtrl.single9);
+        app.get('/single10', indexCtrl.single10);
+        app.get('/single11', indexCtrl.single11);
+        app.get('/single12', indexCtrl.single12);
         app.get('/register', registerCtrl.register);
         
         app.post('/create', registerCtrl.createUser);
